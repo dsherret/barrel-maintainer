659ce4170c852973c298828072220f672e45aa21
David Sherret <dsherret@gmail.com>
2017-12-27 15:57:36 -0500

    chore: Adding command line options.

--- src/cli.ts
+++ src/cli.ts
@@ -1,1 +1,15 @@
 #!/usr/bin/env node
+
+import * as path from "path";
+import {PublicApi} from "./PublicApi";
+import minimist from "minimist";
+
+const cwdPath = path.resolve();
+const argv = minimist(process.argv.slice(2));
+const api = new PublicApi(cwdPath, {
+    quoteType: argv.hasOwnProperty("singleQuotes") ? "'" : "\"",
+    fileExtension: argv.hasOwnProperty("fileExtension") ? argv["fileExtension"] : undefined,
+    includeRootDir: argv.hasOwnProperty("includeRootDir")
+});
+
+api.watchDirectory(cwdPath);
